86d46af2fa1cfda287540442576222fd44e082f7
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-19 13:32:57 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+import { FaAngleDoubleDown } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa";
+import { FaCartShopping, FaChevronDown } from "react-icons/fa6";
+import { CiShoppingCart } from "react-icons/ci";
+
 import Logo from "../assets/website/logo.png";
+import DarkMode from "./DarkMode";
 
 const Menu = [
   {
@@ -15,9 +21,27 @@ const Menu = [
   },
 ];
 
+const DropdownLinks = [
+  {
+    name: "Trending Books",
+    link: "/#",
+  },
+  {
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    name: "Authors",
+    link: "/#",
+  },
+];
+
 const Navbar = () => {
   return (
-    <div>
+    <div
+      className="shadow-lg bg-white dark:bg-gray-900 dark:text-white 
+    duration-200 "
+    >
       <div className="container py-3 sm:py-0">
         <div className="flex justify-between items-center">
           <div>
@@ -26,20 +50,64 @@ const Navbar = () => {
               Books
             </a>
           </div>
-          <div>
+          <div className="flex items-center justify-between gap-4 font-semibold ">
+            <div>
+              <DarkMode />
+            </div>
             <ul className=" items-center gap-4 hidden sm:flex">
               {Menu.map((menu) => (
                 <li key={menu.id}>
                   <a
                     href={menu.link}
                     className="inline-block py-4
-                  px-4 hover:text-primary duration-200"
+                  px-4 hover:text-primary duration-200 dark:hover:text-secondary"
                   >
                     {menu.name}
                   </a>
                 </li>
               ))}
+              {/* dropdown section */}
+              <li className="group relative cursor-pointer">
+                <a href="/#" className="flex items-center h-[72px] gap-[2px]  ">
+                  Quick Links
+                  <span>
+                    <FaChevronDown className="transition duration-300 group-hover:rotate-180" />
+                  </span>
+                </a>
+                {/* dropdown links */}
+                <div
+                  className="absolute -left-9 z-[10] hidden group-hover:block text-black bg-white
+                p-2 shadow-md w-[150px]"
+                >
+                  <ul>
+                    {DropdownLinks.map((link, idx) => (
+                      <li key={idx}>
+                        <a
+                          href={link.link}
+                          className="inline-block
+                        w-full rounded-md p-2 hover:bg-primary/20"
+                        >
+                          {link.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
             </ul>
+            <button
+              className="bg-gradient-to-r
+             from-primary to-secondary
+              text-white px-4 py-1 rounded-full flex items-center gap-3
+              hover:scale-105 duration-300"
+            >
+              Order
+              <CiShoppingCart
+                className="text-2xl 
+              drop-shadow-sm cursor-pointer
+               text-white"
+              />
+            </button>
           </div>
         </div>
       </div>
