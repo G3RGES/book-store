4d566620198988aed896200c1bd386ca4f17d702
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-20 15:33:07 +0200

    Update BestBooks.jsx

--- src/components/BestBooks.jsx
+++ src/components/BestBooks.jsx
@@ -11,6 +11,8 @@ const booksData = [
     title: "Who's there",
     rating: 5.0,
     author: "Someone",
+    description:
+      "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 2,
@@ -18,6 +20,8 @@ const booksData = [
     title: "His Life",
     rating: 4.5,
     author: "John",
+    description:
+      "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 3,
@@ -25,21 +29,27 @@ const booksData = [
     title: "Lost boys",
     rating: 4.7,
     author: "Lost Girl",
+    description:
+      "Lost Boy, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
-  {
-    id: 4,
-    img: Book2,
-    title: "His Life",
-    rating: 4.4,
-    author: "Someone",
-  },
-  {
-    id: 5,
-    img: Book1,
-    title: "Who's There",
-    rating: 4.5,
-    author: "Someone",
-  },
+  //   {
+  //     id: 4,
+  //     img: Book2,
+  //     title: "His Life",
+  //     rating: 4.4,
+  //     author: "Someone",
+  //     description:
+  //       "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   },
+  //   {
+  //     id: 5,
+  //     img: Book1,
+  //     title: "Who's There",
+  //     rating: 4.5,
+  //     author: "Someone",
+  //     description:
+  //       "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   },
 ];
 
 const BestBooks = () => {
@@ -68,11 +78,43 @@ const BestBooks = () => {
           md:gap-5 place-items-center"
           >
             {booksData.map((book) => (
-              <div key={book.id}>
-                <div>
-                  <img src={book.img} alt={book.title} />
+              <div
+                key={book.id}
+                className="rounded-2xl bg-white dark:bg-gray-700/10 
+              hover:bg-primary dark:hover:bg-primary hover:text-white
+              relative shadow-xl duration-500 group max-w-[300px]"
+              >
+                <div className="h-[100px]  ">
+                  <img
+                    src={book.img}
+                    alt={book.title}
+                    className="max-w-[100px] block mx-auto transform -translate-y-14 
+                    group-hover:scale-105 duration-300 shadow-md  "
+                  />
                 </div>
-                <div></div>
+                <div className="p-4 text-center">
+                  <div className="w-full flex items-center justify-center ">
+                    <FaStar className="text-yellow-500" />
+                    <FaStar className="text-yellow-500" />
+                    <FaStar className="text-yellow-500" />
+                    <FaStar className="text-yellow-500" />
+                  </div>
+                  <h1 className="text-xl font-bold">{book.title} </h1>
+                  <p
+                    className="text-gray-500 group-hover:text-white duration-300 text-sm
+                  line-clamp-2 "
+                  >
+                    {book.description}
+                  </p>
+                  <button
+                    className=" bg-primary to-secondary text-white 
+                  px-4 py-2 rounded-full mt-4 hover:scale-105 duration-200
+                   group-hover:bg-white
+                  group-hover:text-primary"
+                  >
+                    Order Now
+                  </button>
+                </div>
               </div>
             ))}
           </div>
