aeb906bfda5b875b12fda99e454ff7583661771f
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-22 13:06:23 +0200

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <div>
+      <div className="container">
+        {/* header */}
+        <div className="text-center mb-20 max-w-[400px] mx-auto ">
+          <p
+            className="text-sm bg-clip-text text-transparent bg-gradient-to-r 
+                  from-primary to-secondary "
+          >
+            What our customers say
+          </p>
+          <h1 className="text-3xl font-bold">Testimonial</h1>
+          <p className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            pellentesque ac.
+          </p>
+        </div>
+        {/* card section */}
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
