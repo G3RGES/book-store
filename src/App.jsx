893ba9779941bb275957b084bb5907a7bb82047f
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-23 11:24:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect } from "react";
+
+// components
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import BestBooks from "./components/BestBooks";
@@ -9,7 +11,21 @@ import TopBooks from "./components/TopBooks";
 import Testimonial from "./components/Testimonial";
 import Footer from "./components/Footer";
 
+// aos
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <>
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
