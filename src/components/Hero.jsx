6c4e46135173e00dffc1ebe9464de38318ad75cb
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-23 12:15:24 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const ImageList = [
   },
 ];
 
-const Hero = () => {
+const Hero = ({ handleOrderPopup }) => {
   const [imageId, setImageId] = useState(Book1);
   const [title, setTitle] = useState("His Life will forever be Changed");
   const [description, setDescription] = React.useState(
@@ -98,6 +98,7 @@ const Hero = () => {
                  from-primary to-secondary rounded-full 
                  px-4 py-2 mt-4 text-white hover:scale-110 
                  duration-300"
+                  onClick={handleOrderPopup}
                 >
                   Order Now
                 </button>
